21161be18097f0c1d1abf1743bd448271f05190f
availe <94802525+availe@users.noreply.github.com>
2024-03-27 17:24:18 -0400

    navbar_mobile_init

--- availe-next-app/components/Navbar/Navbar.client.tsx
+++ availe-next-app/components/Navbar/Navbar.client.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 import Link from "next/link"
 import NavbarListItem from "./NavbarListItem.client"
 import Logo from "@/public/vercel.svg"
-import { AiOutlineMenu } from "react-icons/ai"
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"
 import { useState } from "react"
 
 const Navbar = () => {
@@ -25,10 +25,21 @@ const Navbar = () => {
                         <NavbarListItem href="/contact" text="Contact" />
                     </ul>
                 </div>
-                <div onClick={handleNav} className="md:hidden cursor-pointer pl-24">
-                    <AiOutlineMenu size={25} />
+                <div onClick={handleNav} className="sm:hidden cursor-pointer pl-24">
+                    {isNavOpen ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
                 </div>
             </div>
+
+            {isNavOpen && (
+                <div className="fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+                    <ul className="md:hidden">
+                        <NavbarListItem href="/about" text="Data Science" />
+                        <NavbarListItem href="/about" text="Games" />
+                        <NavbarListItem href="/about" text="About" />
+                        <NavbarListItem href="/contact" text="Contact" />
+                    </ul>
+                </div>
+            )}
         </nav>
     )
 }
