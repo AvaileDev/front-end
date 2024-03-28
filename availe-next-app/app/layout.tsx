d1c2f211a412ce6e50cd82408a4acce8e7dd9b7d
availe <94802525+availe@users.noreply.github.com>
2024-03-27 20:23:37 -0400

    replaced pt with mt

--- availe-next-app/app/layout.tsx
+++ availe-next-app/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        <main className="relative overflow-hidden pt-24">
+        <main className="relative overflow-hidden mt-24">
           {children}
         </main>
         <Footer />
--- availe-next-app/app/page.tsx
+++ availe-next-app/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <p>WIP</p>
+      <div>
+        <p>Availe</p>
+      </div>
     </main>
   );
 }
