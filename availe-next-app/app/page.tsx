658d5bacb4b052963813f79c88bc6170b8416180
availe <94802525+availe@users.noreply.github.com>
2024-03-29 02:00:33 -0400

    about-div

--- availe-next-app/app/page.tsx
+++ availe-next-app/app/page.tsx
@@ -1,29 +1,32 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 import { FiChevronDown } from "react-icons/fi";
-import { SiCplusplus, SiPython, SiTypescript, SiWireshark } from "react-icons/si";
+import { SiCplusplus, SiWireshark } from "react-icons/si";
 import { FaJava, FaAws, FaReact, FaDocker } from "react-icons/fa6";
 import { GoGear } from "react-icons/go";
 import { PiFileSqlLight } from "react-icons/pi";
 import { CiServer } from "react-icons/ci";
 import { TbBrandNextjs } from "react-icons/tb";
+import { TbBrandPython, TbBrandTypescript } from "react-icons/tb";
+
 
 export default function Home() {
   const handleScrollToSection = (id: string) => {
     const section = document.getElementById(id);
     if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
+      section.scrollIntoView({
+        behavior: "smooth",
+      });
     }
   };
 
   const technologies_dict = [
     // Programming Languages
-    { name: 'Python', Icon: SiPython },
+    { name: 'Python', Icon: TbBrandPython },
     { name: 'C++', Icon: SiCplusplus },
     { name: 'Java', Icon: FaJava },
-    { name: 'TypeScript', Icon: SiTypescript },
+    { name: 'TypeScript', Icon: TbBrandTypescript },
 
     // Frontend Technologies
     { name: 'React', Icon: FaReact },
@@ -41,7 +44,7 @@ export default function Home() {
   ];
 
   return (
-    <main className="flex justify-between flex-row min-h-screen bg-blue-400">
+    <><main className="flex justify-between flex-row min-h-screen bg-indigo-400">
       <div className="flex flex-col justify-center flex-grow text-center items-center gap-12">
         <div className="bg-white bg-opacity-20 p-6 rounded-lg shadow-lg">
           <h1 className="font-medium text-white">
@@ -60,14 +63,18 @@ export default function Home() {
         </div>
         <FiChevronDown
           className="text-white text-4xl cursor-pointer mt-5"
-          onClick={() => handleScrollToSection('about-section')}
-        />
-      </div >
+          onClick={() => handleScrollToSection('about-section')} />
+      </div>
       <div className="flex flex-col flex-grow p-10">
         <div className="relative w-full h-full">
           <Image src="/vercel.svg" alt="Vercel Logo" layout="fill" objectFit="contain" />
         </div>
       </div>
-    </main >
+    </main><div id="about-section" className="flex flex-col flex-grow p-10 min-h">
+        <h2 className="text-black text-3xl">About Me</h2>
+        <p className="text-black text-lg">
+          I am a software developer with experience in web development and cloud computing.
+        </p>
+      </div></>
   );
 }
