5c910a1f29171215c53fb162545bd73da395868e
availe <94802525+availe@users.noreply.github.com>
2024-03-29 18:00:18 -0400

    refactor

---
+++ availe-next-app/app/home/HomeMain.tsx
@@ -0,0 +1,66 @@
+import { Main } from 'next/document';
+import React from 'react'
+import { CiServer } from 'react-icons/ci';
+import { FaJava, FaReact, FaAws, FaDocker } from 'react-icons/fa6';
+import { FiChevronDown } from 'react-icons/fi';
+import { GoGear } from 'react-icons/go';
+import { PiFileSqlLight } from 'react-icons/pi';
+import { SiCplusplus, SiWireshark } from 'react-icons/si';
+import { TbBrandPython, TbBrandTypescript, TbBrandNextjs } from 'react-icons/tb';
+import { scrollToSection } from '@/utils/scrollToSection';
+
+const HomeMain = () => {
+    const technologies_dict = [
+        // Programming Languages
+        { name: 'Python', Icon: TbBrandPython },
+        { name: 'C++', Icon: SiCplusplus },
+        { name: 'Java', Icon: FaJava },
+        { name: 'TypeScript', Icon: TbBrandTypescript },
+
+        // Frontend Technologies
+        { name: 'React', Icon: FaReact },
+        { name: 'Next.js', Icon: TbBrandNextjs },
+
+        // Backend and Infrastructure
+        { name: 'AWS', Icon: FaAws },
+        { name: 'Linux', Icon: CiServer },
+        { name: 'Docker', Icon: FaDocker },
+
+        // Development Tools and Databases
+        { name: 'Jenkins', Icon: GoGear },
+        { name: 'SQL', Icon: PiFileSqlLight },
+        { name: 'Wireshark', Icon: SiWireshark },
+    ];
+
+    return (
+        <main className="flex justify-between flex-row min-h-screen bg-indigo-400">
+            <div className="flex flex-col justify-center flex-grow text-center items-center gap-12">
+                <div className="bg-white bg-opacity-20 p-6 rounded-lg shadow-lg">
+                    <h1 className="font-medium text-white">
+                        <span className="text-2xl">Hello, I'm</span> <br />
+                        <span className="text-6xl">Rafael Diaz</span> <br />
+                        <span className="text-3xl">a software developer</span>
+                    </h1>
+                </div>
+                <div className="grid grid-cols-4 gap-4">
+                    {technologies_dict.map((tech, index) => (
+                        <div key={index} className="flex flex-col items-center">
+                            <tech.Icon className="text-white text-4xl" />
+                            <span className="text-white text-lg">{tech.name}</span>
+                        </div>
+                    ))}
+                </div>
+                <FiChevronDown
+                    className="text-white text-4xl cursor-pointer mt-5"
+                    onClick={() => scrollToSection('about-section')} />
+            </div>
+            <div className="flex flex-col flex-grow p-10">
+                <div className="relative w-full h-full">
+                    <img src="/vercel.svg" alt="Vercel Logo" style={{ objectFit: 'contain' }} />
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default HomeMain
--- availe-next-app/app/page.tsx
+++ availe-next-app/app/page.tsx
@@ -1,80 +1,18 @@
 "use client";
 
-import Image from "next/image";
-import { FiChevronDown } from "react-icons/fi";
-import { SiCplusplus, SiWireshark } from "react-icons/si";
-import { FaJava, FaAws, FaReact, FaDocker } from "react-icons/fa6";
-import { GoGear } from "react-icons/go";
-import { PiFileSqlLight } from "react-icons/pi";
-import { CiServer } from "react-icons/ci";
-import { TbBrandNextjs } from "react-icons/tb";
-import { TbBrandPython, TbBrandTypescript } from "react-icons/tb";
+import HomeMain from "./home/HomeMain";
 
 
 export default function Home() {
-  const handleScrollToSection = (id: string) => {
-    const section = document.getElementById(id);
-    if (section) {
-      section.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-  };
-
-  const technologies_dict = [
-    // Programming Languages
-    { name: 'Python', Icon: TbBrandPython },
-    { name: 'C++', Icon: SiCplusplus },
-    { name: 'Java', Icon: FaJava },
-    { name: 'TypeScript', Icon: TbBrandTypescript },
-
-    // Frontend Technologies
-    { name: 'React', Icon: FaReact },
-    { name: 'Next.js', Icon: TbBrandNextjs },
-
-    // Backend and Infrastructure
-    { name: 'AWS', Icon: FaAws },
-    { name: 'Linux', Icon: CiServer },
-    { name: 'Docker', Icon: FaDocker },
-
-    // Development Tools and Databases
-    { name: 'Jenkins', Icon: GoGear },
-    { name: 'SQL', Icon: PiFileSqlLight },
-    { name: 'Wireshark', Icon: SiWireshark },
-  ];
-
   return (
-    <><main className="flex justify-between flex-row min-h-screen bg-indigo-400">
-      <div className="flex flex-col justify-center flex-grow text-center items-center gap-12">
-        <div className="bg-white bg-opacity-20 p-6 rounded-lg shadow-lg">
-          <h1 className="font-medium text-white">
-            <span className="text-2xl">Hello, I'm</span> <br />
-            <span className="text-6xl">Rafael Diaz</span> <br />
-            <span className="text-3xl">a software developer</span>
-          </h1>
-        </div>
-        <div className="grid grid-cols-4 gap-4">
-          {technologies_dict.map((tech, index) => (
-            <div key={index} className="flex flex-col items-center">
-              <tech.Icon className="text-white text-4xl" />
-              <span className="text-white text-lg">{tech.name}</span>
-            </div>
-          ))}
-        </div>
-        <FiChevronDown
-          className="text-white text-4xl cursor-pointer mt-5"
-          onClick={() => handleScrollToSection('about-section')} />
-      </div>
-      <div className="flex flex-col flex-grow p-10">
-        <div className="relative w-full h-full">
-          <Image src="/vercel.svg" alt="Vercel Logo" layout="fill" objectFit="contain" />
-        </div>
-      </div>
-    </main><div id="about-section" className="flex flex-col flex-grow p-10 min-h">
+    <>
+      <HomeMain></HomeMain>
+      <div id="about-section" className="flex flex-col flex-grow p-10 min-h">
         <h2 className="text-black text-3xl">About Me</h2>
         <p className="text-black text-lg">
           I am a software developer with experience in web development and cloud computing.
         </p>
-      </div></>
+      </div>
+    </>
   );
 }
