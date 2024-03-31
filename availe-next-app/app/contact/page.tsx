093da6a0bc51d443ffa66e5c334caa366d60cd07
availe <94802525+availe@users.noreply.github.com>
2024-03-30 20:37:21 -0400

    subject

--- availe-next-app/app/contact/page.tsx
+++ availe-next-app/app/contact/page.tsx
@@ -5,6 +5,7 @@ export default function Contact() {
         <form className="flex flex-col gap-4 mb-8 items-center">
             <h2 className="text-lg">Contact Form</h2>
             <input type="email" name="Email" placeholder="Email" className={inputClass} />
+            <input type="text" name="Subject" placeholder="Subject" className={inputClass} />
             <textarea name="Message" rows={14} placeholder="Message" className={inputClass} />
             <button type="submit" className="w-1/5 self-center bg-blue-500 hover::bg-blue-700 text-white font-medium py-2 rounded">Submit</button>
         </form>
