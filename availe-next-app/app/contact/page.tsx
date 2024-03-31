b85a07398a0134e05086b2459a57cfb190f5fe77
availe <94802525+availe@users.noreply.github.com>
2024-03-31 10:33:29 -0400

    contact_styling

--- availe-next-app/app/contact/page.tsx
+++ availe-next-app/app/contact/page.tsx
@@ -1,26 +1,26 @@
 export default function Contact() {
-  const inputClass =
-    "border border-gray-300 w-3/5 rounded px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-300 ease-in-out";
+  const inputClass = "border border-gray-300 w-3/5 rounded px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-300 ease-in-out self-center";
+  const labelClass = "block text-gray-600 text-sm font-medium mb-2";
 
   return (
-    <form className="flex flex-col gap-4 items-center">
+    <form className="flex flex-col gap-4 items-center relative border rounded p-3 min-h-screen justify-center">
       <h2 className="text-lg">Contact Form</h2>
+      <label htmlFor="Email" className={labelClass}>Email</label>
       <input
         type="email"
         name="Email"
-        placeholder="Email"
         className={inputClass}
       />
+      <label htmlFor="Subject" className={labelClass}>Subject</label>
       <input
         type="text"
         name="Subject"
-        placeholder="Subject"
         className={inputClass}
       />
+      <label htmlFor="Message" className={labelClass}>Message</label>
       <textarea
         name="Message"
         rows={14}
-        placeholder="Message"
         className={inputClass}
       />
       <button
