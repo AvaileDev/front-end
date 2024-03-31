f2316c8e64f456ae9efdb5b91d7ab20b8a7b8e35
availe <94802525+availe@users.noreply.github.com>
2024-03-30 20:15:20 -0400

    contact_init

--- availe-next-app/app/contact/page.tsx
+++ availe-next-app/app/contact/page.tsx
@@ -1,8 +1,39 @@
 export default function Contact() {
     return (
-        <><div>
-            <p>Hello contact</p>
-        </div>
-
-        </>)
+        <form className="flex flex-col w-1/2 mx-auto">
+            <div className="flex flex-row justify-evenly">
+                <div className="flex flex-col">
+                    <label htmlFor="firstName">First Name</label>
+                    <input
+                        type="text"
+                        id="firstName"
+                        className="border-b border-black bg-transparent"
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="name">Last Name</label>
+                    <input
+                        type="text"
+                        id="lastName"
+                        className="border-b border-black bg-transparent"
+                    />
+                </div>
+            </div>
+            <label htmlFor="email">Email:</label>
+            <input
+                type="email"
+                id="email"
+                className="border-b border-black bg-transparent"
+            />
+            <label htmlFor="Subject">Subject:</label>
+            <input
+                type="text"
+                id="subject"
+            />
+            <label htmlFor="message">Message:</label>
+            <textarea
+                id="message"
+            />
+        </form>
+    )
 }
