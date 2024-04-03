ddda34ae055c42c7296398183a815e4c8f5308ff
availe <94802525+availe@users.noreply.github.com>
2024-04-03 16:25:05 -0400

    about

--- availe-next-app/app/about/page.tsx
+++ availe-next-app/app/about/page.tsx
@@ -1,13 +1,24 @@
 export default function About() {
   return (
-    <div className="max-w-xl mx-auto">
+    <div className="min-h-screen-minus-footer mx-auto px-10 max-w-xl flex flex-col gap-10">
+      <h1 className="text-2xl font-semibold">About Me</h1>
       <p>
-        When I started my journey in computer science, I did not know much about
-        the field. At the time, my interests lay in robotics. And compared to
-        moving physical components, software was a sort of untouchable black
-        box. But as I delved deeper, I came to see software less as a necessary
-        obstacle and more so as a
+        Hello! I'm a senior computer science student who's deeply passionate about all things CS-related. I have real-world experience operating Linux systems in an internship environment, and I have taken elective courses in microcontrollers and network protocols. In the upcoming semester, I will also be taking an elective in digital signal processing.
       </p>
+
+      <div className="flex flex-col gap-4">
+        <p>For this website, I used the following tech stack:</p>
+        <ul className="list-disc list-inside text-gray-700 text-lg">
+          <li>Front-end: React, Typescript, Tailwind</li>
+          <li>Back-end: NextJS</li>
+          <li>CI/CD: Jenkins, Docker</li>
+          <li>Hosting: EC2 AWS</li>
+        </ul>
+
+        <p className="border-t border-gray-300 pt-6">
+          Note that this website is a work in progress. I'm currently focusing on remastering projects in a web-deployable format.
+        </p>
+      </div>
     </div>
   );
 }
