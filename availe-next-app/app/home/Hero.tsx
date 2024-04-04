488a01caa7bde82cd4f4cfb77426d271ee0ea469
availe <94802525+availe@users.noreply.github.com>
2024-04-04 00:18:43 -0400

    refactor_tabs

--- availe-next-app/app/home/Hero.tsx
+++ availe-next-app/app/home/Hero.tsx
@@ -1,43 +1,11 @@
-import { CiServer } from "react-icons/ci";
-import { FaJava, FaReact, FaAws, FaDocker } from "react-icons/fa6";
 import { FiChevronDown } from "react-icons/fi";
-import { GoGear } from "react-icons/go";
-import { PiFileSqlLight } from "react-icons/pi";
-import { SiCplusplus, SiWireshark } from "react-icons/si";
-import {
-  TbBrandPython,
-  TbBrandTypescript,
-  TbBrandNextjs,
-} from "react-icons/tb";
 import { scrollToSection } from "@/utils/scrollToSection";
 import Link from "next/link";
 import Image from "next/image";
-import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import HeroTabs from "./HeroTabs";
 
 
 const HomeMain = () => {
-  const technologies_dict = [
-    // Programming Languages
-    { name: "Python", Icon: TbBrandPython },
-    { name: "C++", Icon: SiCplusplus },
-    { name: "Java", Icon: FaJava },
-    { name: "TypeScript", Icon: TbBrandTypescript },
-
-    // Frontend Technologies
-    { name: "React", Icon: FaReact },
-    { name: "Next.js", Icon: TbBrandNextjs },
-
-    // Backend and Infrastructure
-    { name: "AWS", Icon: FaAws },
-    { name: "Linux", Icon: CiServer },
-    { name: "Docker", Icon: FaDocker },
-
-    // Development Tools and Databases
-    { name: "Jenkins", Icon: GoGear },
-    { name: "SQL", Icon: PiFileSqlLight },
-    { name: "Wireshark", Icon: SiWireshark },
-  ];
-
   return (
     <div className="flex justify-evenly flex-col min-h-screen bg-indigo-400">
       <div className="flex flex-row">
@@ -52,35 +20,7 @@ const HomeMain = () => {
             </div>
           </Link>
 
-          <Tabs defaultValue="languages" className="w-full max-w-md mx-auto">
-            <TabsList aria-label="Manage your settings">
-              <TabsTrigger value="languages">Languages</TabsTrigger>
-              <TabsTrigger value="frontend">Frontend</TabsTrigger>
-              <TabsTrigger value="infrastructure">Infrastructure</TabsTrigger>
-              <TabsTrigger value="devtools">Development Tools</TabsTrigger>
-            </TabsList>
-            <TabsContent value="languages">
-              <p>Languages</p>
-            </TabsContent>
-            <TabsContent value="frontend">
-              <p>Frontend</p>
-            </TabsContent>
-            <TabsContent value="infrastructure">
-              <p>Infrastructure</p>
-            </TabsContent>
-            <TabsContent value="devtools">
-              <p>Development Tools</p>
-            </TabsContent>
-          </Tabs>
-
-          {/* <div className="grid grid-cols-4 gap-0 md:gap-4">
-            {technologies_dict.map((tech, index) => (
-              <div key={index} className="flex flex-col items-center">
-                <tech.Icon className="text-white text-4xl" />
-                <span className="text-white text-lg">{tech.name}</span>
-              </div>
-            ))}
-          </div> */}
+          <HeroTabs></HeroTabs>
         </div>
         <div className="flex-col flex-grow w-2/5 p-10 hidden md:flex">
           <h1 className="text-white text-2xl text-center mb-5">
@@ -112,3 +52,13 @@ const HomeMain = () => {
 };
 
 export default HomeMain;
+
+
+{/* <div className="grid grid-cols-4 gap-0 md:gap-4">
+            {technologies_dict.map((tech, index) => (
+              <div key={index} className="flex flex-col items-center">
+                <tech.Icon className="text-white text-4xl" />
+                <span className="text-white text-lg">{tech.name}</span>
+              </div>
+            ))}
+          </div> */}
---
+++ availe-next-app/app/home/HeroTabs.tsx
@@ -0,0 +1,63 @@
+import { CiServer } from "react-icons/ci";
+import { FaJava, FaReact, FaAws, FaDocker } from "react-icons/fa6";
+import { FiChevronDown } from "react-icons/fi";
+import { GoGear } from "react-icons/go";
+import { PiFileSqlLight } from "react-icons/pi";
+import { SiCplusplus, SiWireshark } from "react-icons/si";
+import {
+    TbBrandPython,
+    TbBrandTypescript,
+    TbBrandNextjs,
+} from "react-icons/tb";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+
+const technologies_dict = [
+    // Programming Languages
+    { name: "Python", Icon: TbBrandPython },
+    { name: "C++", Icon: SiCplusplus },
+    { name: "Java", Icon: FaJava },
+    { name: "TypeScript", Icon: TbBrandTypescript },
+
+    // Frontend Technologies
+    { name: "React", Icon: FaReact },
+    { name: "Next.js", Icon: TbBrandNextjs },
+
+    // Backend and Infrastructure
+    { name: "AWS", Icon: FaAws },
+    { name: "Linux", Icon: CiServer },
+    { name: "Docker", Icon: FaDocker },
+
+    // Development Tools and Databases
+    { name: "Jenkins", Icon: GoGear },
+    { name: "SQL", Icon: PiFileSqlLight },
+    { name: "Wireshark", Icon: SiWireshark },
+];
+
+const HeroTabs = () => {
+    return (
+        <Tabs defaultValue="languages" className="w-full max-w-md mx-auto">
+            <TabsList aria-label="Manage your settings">
+                <TabsTrigger value="languages">Languages</TabsTrigger>
+                <TabsTrigger value="frontend">Frontend</TabsTrigger>
+                <TabsTrigger value="infrastructure">Infrastructure</TabsTrigger>
+                <TabsTrigger value="devtools">Development Tools</TabsTrigger>
+            </TabsList>
+            <TabsContent value="languages">
+                <p>Languages</p>
+            </TabsContent>
+            <TabsContent value="frontend">
+                <p>Frontend</p>
+            </TabsContent>
+            <TabsContent value="infrastructure">
+                <p>Infrastructure</p>
+            </TabsContent>
+            <TabsContent value="devtools">
+                <p>Development Tools</p>
+            </TabsContent>
+        </Tabs>
+
+    )
+}
+
+
+export default HeroTabs
