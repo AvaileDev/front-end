47a495eb226e73e08068ff44b298caf96cee2b4f
availe <94802525+availe@users.noreply.github.com>
2024-04-20 12:54:22 -0400

    hero tabs update

--- app/home/HeroTabs.tsx
+++ app/home/HeroTabs.tsx
@@ -1,15 +1,16 @@
 import { CiServer } from "react-icons/ci";
 import { FaJava, FaReact, FaAws, FaDocker } from "react-icons/fa6";
-import { FiChevronDown } from "react-icons/fi";
 import { GoGear } from "react-icons/go";
 import { PiFileSqlLight } from "react-icons/pi";
-import { SiCplusplus, SiWireshark } from "react-icons/si";
+import { SiCplusplus, SiWireshark, SiPostgresql } from "react-icons/si";
 import {
     TbBrandPython,
     TbBrandTypescript,
     TbBrandNextjs,
+    TbBrandPrisma
 } from "react-icons/tb";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import DataScience from "../data/page";
 
 const iconMap = {
     Languages: [
@@ -19,20 +20,22 @@ const iconMap = {
         { name: "TypeScript", Icon: TbBrandTypescript },
     ],
 
-    Frontend: [
+    WebDevelopment: [
         { name: "React", Icon: FaReact },
         { name: "Next.js", Icon: TbBrandNextjs },
-    ],
-
-    Infrastructure: [
         { name: "AWS", Icon: FaAws },
         { name: "Linux", Icon: CiServer },
         { name: "Docker", Icon: FaDocker },
+        { name: "Jenkins", Icon: GoGear },
+        { name: "Postgres", Icon: SiPostgresql },
+        { name: "Prisma", Icon: TbBrandPrisma }
     ],
 
-    DevTools: [
-        { name: "Jenkins", Icon: GoGear },
+    DataScience: [
         { name: "SQL", Icon: PiFileSqlLight },
+    ],
+
+    Tools: [
         { name: "Wireshark", Icon: SiWireshark },
     ]
 };
@@ -42,12 +45,14 @@ const HeroTabs = () => {
         <Tabs defaultValue="Languages" className="w-full max-w-md mx-auto">
             <TabsList aria-label="Manage your settings">
                 {Object.keys(iconMap).map((category) => (
-                    <TabsTrigger key={category} value={category} className="text-indigo-100 border-indigo-100 data-[state=active]:border-white data-[state=active]:text-white">{category}</TabsTrigger>
+                    <TabsTrigger key={category} value={category} className="text-indigo-100 data-[state=active]:border-white data-[state=active]:text-white">
+                        {category.replace(/([A-Z])/g, ' $1').trim()}
+                    </TabsTrigger>
                 ))}
             </TabsList>
 
             {Object.entries(iconMap).map(([category, iconsList]) => (
-                <TabsContent key={category} value={category}>
+                <TabsContent key={category} value={category} className="min-h-[150px]">
                     <div className="grid grid-cols-4 gap-4 mt-8">
                         {iconsList.map((tech, index) => (
                             <div key={index} className="flex flex-col items-center">
