4454670759de8f34c84d3d2cea856f79f763b5c7
availe <94802525+availe@users.noreply.github.com>
2024-04-20 20:55:28 -0400

    contact form with api

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,58 @@
+'use client'
+import React from "react";
+
 export default function Contact() {
   const inputClass =
     "w-full border border-gray-300 rounded px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-300 ease-in-out self-center shadow-sm hover:shadow-md";
   const labelClass = "text-left block text-gray-600 text-sm font-medium mb-2";
 
+  const [loading, setLoading] = React.useState(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setLoading(true)
+
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const data = {
+      email: formData.get("Email"),
+      subject: formData.get("Subject"),
+      content: formData.get("Message"),
+    };
+
+    if (!data.email || !data.subject || !data.content) {
+      alert("Please fill all fields.");
+      return;
+    }
+
+    try {
+      const res = await fetch("/api/email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (res.ok) {
+        form.reset();
+        alert("Email sent successfully.");
+      }
+    }
+    catch {
+      alert("Error occured while sending email.");
+    }
+    finally {
+      setLoading(false);
+    }
+  }
+
+
   return (
     <div className="w-full">
-      <form className="flex flex-col gap-6 w-2/5 mx-auto">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-6 w-2/5 mx-auto">
         <h2 className="text-lg text-center">Contact Form</h2>
         <label htmlFor="Email" className={labelClass}>
           Email
@@ -22,8 +69,9 @@ export default function Contact() {
         <button
           type="submit"
           className="w-full md:w-2/5 self-center bg-blue-500 text-white font-medium py-2 rounded hover:brightness-95"
+          disabled={loading}
         >
-          Submit
+          {loading ? 'Sending...' : 'Submit'}
         </button>
       </form>
     </div>
