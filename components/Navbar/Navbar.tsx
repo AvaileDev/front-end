8f4520f3660f12332ab81153505c40e1a5a2c58b
availe <94802525+availe@users.noreply.github.com>
2024-04-21 22:55:12 -0400

    mobile navbar useEffect

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -14,10 +14,25 @@ const Navbar = () => {
   const router = useRouter();
   const pathname = usePathname();
 
+  const menuRef = useRef<HTMLDivElement>(null);
+  const handleOutsideClick = (e: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+      console.log("click outside");
+      setIsNavOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+
   const menuClasses = `fixed right-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 
                          transform ${isNavOpen ? "-translate-x-0" : "translate-x-full"} 
                          transition-transform duration-500 ease-in-out`;
-  const closeIconClasses = `absolute top-6 left-4 transition-opacity duration-0 ease-in-out ${isNavOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`;
+  const closeIconClasses = `absolute top-6 right-4 transition-opacity duration-0 ease-in-out ${isNavOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`;
 
   const nav_dict = [
     { href: "/", text: "Home" },
@@ -30,9 +45,13 @@ const Navbar = () => {
   return (
     <nav className="sticky top-0 w-full bg-white z-10">
       <div className="flex justify-between items-center h-full w-full px-4 2xl:px-16">
+
+        {/* Logo */}
         <Link href="/">
           <Image src={Logo} alt="Logo" width={205} height={75} />
         </Link>
+
+        {/* Desktop navbar */}
         <div>
           <ul className="hidden sm:flex mr-1">
             {nav_dict.map(({ href, text }, index) => (
@@ -44,13 +63,15 @@ const Navbar = () => {
           <AiOutlineMenu size={25} />
         </div>
       </div>
-      <div className={menuClasses}>
+
+      {/* Mobile navbar */}
+      <div className={menuClasses} ref={menuRef}>
         <div className={closeIconClasses} onClick={handleNav}>
           {isNavOpen ? <AiOutlineClose size={25} /> : null}
         </div>
         <ul className="flex flex-col gap-y-4 py-4">
           {nav_dict.map(({ href, text }, index) => (
-            <NavbarListItem key={index} href={href} text={text} isActive={pathname === href} />
+            <NavbarListItem key={index} href={href} text={text} isActive={pathname === href} onItemClick={handleNav} />
           ))}
         </ul>
       </div>
