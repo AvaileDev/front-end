ed379fe1e0a4b493532215fd5c27ff33010dac3f
availe <94802525+availe@users.noreply.github.com>
2024-05-05 19:51:17 -0400

    Update page.tsx

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -9,7 +9,7 @@ export default function GameDev() {
   return (
     <>
       <div className='sticky top-11 w-full bg-white z-10'>
-        <p>Game WIP, no defeat/victory condition or mobile support yet. Use WASD keys to move: W to jump and S to fall rapidly. Press space bar to jump very far.</p>
+        <p>Game WIP, no defeat/victory condition or mobile support yet. Use WASD keys to move: W to jump and S to fall rapidly. Press shift to jump very far.</p>
       </div>
       <div className="flex flex-col h-screen">
         <GameIframe></GameIframe>
