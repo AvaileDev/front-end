ac8ddf3a54645da5bdff5a6ee27d743e80448393
availe <94802525+availe@users.noreply.github.com>
2024-05-05 19:48:37 -0400

    Update page.tsx

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -8,7 +8,10 @@ const GameIframe = dynamic(() => import('@/components/Game'), {
 export default function GameDev() {
   return (
     <>
-      <div className="h-screen">
+      <div className='sticky top-11 w-full bg-white z-10'>
+        <p>Game WIP, no defeat/victory condition or mobile support yet. Use WASD keys to move: W to jump and S to fall rapidly. Press space bar to jump very far.</p>
+      </div>
+      <div className="flex flex-col h-screen">
         <GameIframe></GameIframe>
       </div>
     </>
